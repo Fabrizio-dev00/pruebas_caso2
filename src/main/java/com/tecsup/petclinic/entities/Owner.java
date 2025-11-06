13f6a37db35140f66c0df3cebbd15c54ab821677
Anderson Rivera <anderson.rivera@tecsup.edu.pe>
2025-11-06 16:39:19 -0500

    Ownerr

---
+++ src/main/java/com/tecsup/petclinic/entities/Owner.java
@@ -0,0 +1,32 @@
+package com.tecsup.petclinic.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "owners")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephone")
+    private String telephone;
+}
